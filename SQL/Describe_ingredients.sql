8772c7bfbf6a0b9bf331bf578d3b5e9a8b1f3f70
neit-H91 <zking4540@gmail.com>
2025-07-29 16:59:17 +0200

    describe ingr

--- SQL/Describe_ingredients.sql
+++ SQL/Describe_ingredients.sql
@@ -9,77 +9,53 @@ declare
   current_oid int;
   description text;
   ingredient_counts record;
-  ingredient_list text := '';
-  get_orders cursor for select distinct order_id from customer_orders;
+  ingredient_list int[];
+  get_orders cursor for select distinct order_id from customer_orders order by order_id asc;
+  current_ingredient text;
+  current_ingredient_count int;
 begin
-  open get_orders;
+open get_orders;
+loop
+fetch get_orders into current_oid;
+exit when not found;
+raise notice 'Order number - %', current_oid;
+select into ingredient_list array_agg(ing) as ingredients
+from (
+  select unnest(
+    array_cat(
+      array(
+        select unnest(string_to_array(toppings, ',')::int[])
+        except
+        select unnest(string_to_array(exclusions, ',')::int[])
+      ),
+      string_to_array(extras, ',')::int[]
+    )
+  ) as ing
+  from customer_orders co
+  join pizza_recipes pr
+    on co.pizza_id = pr.pizza_id
+  where order_id = current_oid
+) sub;
+description :='Ingredients : ';
+  for current_ingredient_count, current_ingredient in
+    select count(*), pt.topping_name
+    from unnest(ingredient_list) as t(val)
+    join pizza_toppings pt on t.val = pt.topping_id
+    group by pt.topping_name
+    order by pt.topping_name asc
   loop
-    fetch get_orders into current_oid;
-    exit when not found;
-
-    -- Pour chaque order_id, on récupère les ingrédients filtrés et comptés
-    ingredient_list := (
-      select string_agg(counts || ' ' || name, ', ' order by name)
-      from (
-        select count(*) as counts, pt.topping_name as name
-        from (
-          select unnest(array_cat(
-            string_to_array(pr.toppings, ',')::int[],
-            string_to_array(co.extras, ',')::int[]
-          )) as topping_id,
-          string_to_array(co.exclusions, ',')::int[] as exclusions
-          from customer_orders co
-          join pizza_recipes pr on co.pizza_id = pr.pizza_id
-          where co.order_id = current_oid
-        ) sub
-        join pizza_toppings pt on pt.topping_id = sub.topping_id
-        where not topping_id = any(sub.exclusions)
-        group by pt.topping_name
-      ) counts
-    );
-
-    description := 'Order ' || current_oid || ': ' || coalesce(ingredient_list, 'No ingredients');
-
-    raise notice '%', description;
+	if current_ingredient_count > 1 then
+	  description := description || current_ingredient || ' x' || current_ingredient_count || ', ';
+	else
+	  description := description || current_ingredient || ', ';
+	end if;
   end loop;
-  close get_orders;
+  raise notice '%', description;
+
+
+end loop;
+close get_orders;
 end;
 $$;
 
-select order_id from customer_orders;
-
-select *
-from (
-  select 
-    string_to_array(concat_ws(',', toppings, extras),',') as ingredients,
-    exclusions
-  from customer_orders co
-  join pizza_recipes pr on pr.pizza_id = co.pizza_id
-  where order_id = 4
-) sub;
-
-select
-  co.order_id,
-  unnest(
-    string_to_array(concat_ws(',', toppings, extras),',')
-  ) as ingredient
-from customer_orders co
-join pizza_recipes pr on pr.pizza_id = co.pizza_id
-where co.order_id = 4;
--- and not (
---   unnest(
---     string_to_array(concat_ws(',', toppings, extras),',')
---   ) = any(string_to_array(co.exclusions, ','))
--- )
-
-with test_ingredients as (
-	select
-	  co.order_id,
-	  array_agg(unnest_ingredients) as ingredients
-	from customer_orders co
-	join pizza_recipes pr on pr.pizza_id = co.pizza_id
-	cross join lateral unnest(
-	    string_to_array(concat_ws(',', pr.toppings, co.extras), ',')
-	) as t(unnest_ingredients)
-	group by co.order_id;
-)
+select describe_ingredients();
--- SQL/ingredients.sql
+++ SQL/ingredients.sql
@@ -1,5 +1,6 @@
 SET search_path TO pizza_runner;
 
+with ingredients_per_order as(
 select 
 array_cat(
 array(
@@ -8,9 +9,55 @@ array(
   select unnest(string_to_array(exclusions, ',')::int[])
 ),
 string_to_array(extras, ',')::int[]
-) as ingredients,
-order_id,co.pizza_id
+) as ingredients
 from customer_orders co
 join pizza_recipes pr
 on co.pizza_id = pr.pizza_id
-order by order_id,pizza_id;
+where order_id = 4
+)
+
+select 
+  i.ingredient_id,
+  pt.topping_name,
+  count(*) as nb,
+  ipo.order_id
+from ingredients_per_order ipo
+cross join lateral unnest(ipo.ingredients) as i(ingredient_id)
+join pizza_toppings pt
+  on pt.topping_id = i.ingredient_id
+group by order_id, pt.topping_name
+order by nb desc;
+
+
+select array_agg(ing) as ingredients
+from (
+  select unnest(
+    array_cat(
+      array(
+        select unnest(string_to_array(toppings, ',')::int[])
+        except
+        select unnest(string_to_array(exclusions, ',')::int[])
+      ),
+      string_to_array(extras, ',')::int[]
+    )
+  ) as ing
+  from customer_orders co
+  join pizza_recipes pr
+    on co.pizza_id = pr.pizza_id
+  where order_id = 1
+) sub;
+
+select val, count(*), tp.topping_name
+from unnest(array[8,10,1,5,2,6,3,8,10,1,5,2,6,3,11,9,7,6,12]) as t(val)
+join pizza_toppings
+on t.val = pt.topping_id
+group by t.val, topping_name
+order by count(*) desc;
+
+select count(*), pt.topping_name
+from unnest(array[8,10,1,5,2,6,3,8,10,1,5,2,6,3,11,9,7,6,12]) as t(val)
+join pizza_toppings pt
+  on t.val = pt.topping_id
+group by pt.topping_name
+order by topping_name asc;
+
